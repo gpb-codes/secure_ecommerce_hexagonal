e0485f77259797c19e5165f39d439d7a2b8b5cfd
bellcranel7047 <zunigaangel8abs@gmail.com>
2026-01-09 09:49:24 -0300

    Update create_product.py

--- src/application/use_cases/create_product.py
+++ src/application/use_cases/create_product.py
@@ -1,1 +1,8 @@
 
+class CreateProduct:
+    def __init__(self, repository: ProductRepository):
+        self.repository = repository
+
+    def execute(self, name: str, price: float, stock: int) -> Product:
+        product = Product.create(name, price, stock)
+        return self.repository.save(product)
